cee5e8ece415583cde343718b5562fdb15918a92
tanyp <typ1805@qq.com>
2021-12-09 17:57:05 +0800

    更新

--- tansci-view/src/views/Login.vue
+++ tansci-view/src/views/Login.vue
@@ -1,31 +1,47 @@
 <template>
 	<div class="login" :style="login">
-		<el-form class="login-form" :model="loginForm" :rules="rules" ref="loginRuleForm">
-			<el-form-item prop="username" :rules="[
+		<div class="login-main">
+			<div></div>	
+			<el-form class="login-form" :model="loginForm" :rules="rules" ref="loginRuleForm">
+				<div class="login-form-title">欢迎登录</div>
+				<el-form-item prop="username" :rules="[
 						{required: true,message: '请输入用户名',trigger: 'blur'},
 						{pattern: /^[a-zA-Z]\w{4,17}$/,message: '用户名式有误，请重新输入',trigger: 'blur'}]">
-				<el-input v-model="loginForm.username" prefix-icon="el-icon-user" placeholder="请输入用户名称" style="width:100%"></el-input>
-			</el-form-item>
-			<el-form-item prop="password" :rules="[
+					<el-input v-model="loginForm.username" :prefix-icon="Avatar" placeholder="请输入用户名称" style="width:100%">
+						<template #prefix>
+							<el-icon :size="20" style="padding-top:6px;"><Avatar/></el-icon>
+						</template>
+					</el-input>
+				</el-form-item>
+				<el-form-item prop="password" :rules="[
 						{required: true,message: '请输入密码',trigger: 'blur'},
 						{pattern: /^[a-zA-Z]\w{5,17}$/,message: '密码格式有误，请重新输入',trigger: 'blur'}]">
-				<el-input type="password" v-model="loginForm.password" prefix-icon="el-icon-lock" show-password placeholder="请输入密码" style="width:100%"></el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="text">忘记密码？</el-button>
-			</el-form-item>
-			<el-form-item>
-				<el-button round @click="submit" style="width:100%">登录</el-button>
-			</el-form-item>
-		</el-form>
+					<el-input type="password" v-model="loginForm.password" :prefix-icon="Lock" show-password placeholder="请输入密码" style="width:100%">
+						<template #prefix>
+							<el-icon :size="20" style="padding-top:6px;"><Lock/></el-icon>
+						</template>
+					</el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="text">忘记密码？</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" round @click="submit" style="width:100%">登录</el-button>
+				</el-form-item>
+			</el-form>
+		</div>
 	</div>
 </template>
 <script>
 	import {onBeforeMount,reactive,ref,toRefs,unref} from "vue"
+	import {Avatar,Lock} from "@element-plus/icons"
 	import {useRouter} from 'vue-router'
 	import {useStore} from 'vuex'
 	import {login} from '../api/systemApi'
 	export default {
+		components:{
+			Avatar,Lock,
+		},
 		setup() {
 			const store = useStore()
 			const router = useRouter()
@@ -71,19 +87,29 @@
 </script>
 <style lang="less">
 	.login {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
 		background-image: url('../assets/image/login-bg.jpg');
 		background-size: 100% 100%;
-		.login-form{
-			padding: 4rem 2rem;
-			background: rgba(0, 0, 0, 0.2);
-			width: 20rem;
-			border-radius: 1.2rem;
-			border: 1px solid #fff;
-			box-shadow: 0px 0px 20px 4px #fff;
+		.login-main{
+			height: 100%;
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-around;
+			align-items: center;
+			.login-form{
+				padding: 2rem;
+				background: rgba(0, 0, 0, 0.5);
+				width: 20rem;
+				border-radius: 1.2rem;
+				border: 1px solid #fff;
+				box-shadow: 0px 0px 20px 4px #fff;
+				.login-form-title{
+					color: rgb(255, 255, 255);
+					text-align: center;
+					font-size: 18px;
+					font-weight: 700;
+					padding-bottom: 2rem;
+				}
+			}
 		}
 	}
 </style>
