80b929c27374d7d7c45bfd8d2c93cdc163a74b16
tanyp <typ1805@qq.com>
2021-12-14 16:45:39 +0800

    表格组件

--- tansci-view/src/components/Table.vue
+++ tansci-view/src/components/Table.vue
@@ -1,34 +1,55 @@
 <template>
     <div class="table-container">
         <div class="search-wrap">
-            <slot></slot>
+            <slot name="search"></slot>
         </div>
         <div class="table-wrap">
             <el-table :data="data" border stripe size="mini" :height="tableHeight" row-key="id" :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
-                v-loading="loading" :header-cell-style="{background:'var(--bg3)'}" style="width: 100%;"
-                @selection-change="onSelectionChange" :cell-style="cellStyle">
+                v-loading="loading" :header-cell-style="headerCellStyle" :cell-style="cellStyle" 
+                @selection-change="onSelectionChange" style="width: 100%;">
                 <template v-for="item in column" :key="item">
                     <el-table-column v-if="!item.prop && !item.label" :fixed="item.fixed" type="selection" width="45"></el-table-column>
+                    <!-- el-tag -->
                     <el-table-column v-else-if="item.type == 'tag'" show-overflow-tooltip
                         :label="item.label" :align="item.align != null ? item.align : 'center'" :width="item.width">
                         <template #default="scope">
                             <el-tag :size="item.option.size" 
                                 :effect="item.option.effect" 
                                 :type="item.option.type">
-                                {{valFormat(scope.row,item.prop)}}    
+                                {{scope.row[item.prop]}}    
                             </el-tag>
                         </template>
                     </el-table-column>
+                    <!-- el-button -->
+                    <el-table-column v-else-if="item.type == 'button'" show-overflow-tooltip
+                        :label="item.label" :align="item.align != null ? item.align : 'center'" :width="item.width">
+                        <template #default="scope">
+                            <el-button @click="$emit('onButtonClick',scope.row)" type="text" :size="item.option.size">
+                                {{scope.row[item.prop]}}    
+                            </el-button>
+                        </template>
+                    </el-table-column>
+                    <!-- el-switch -->
                     <el-table-column v-else-if="item.type == 'switch'" show-overflow-tooltip 
                         :label="item.label" :align="item.align != null ? item.align : 'center'" :width="item.width">
                         <template #default="scope">
-                            <el-switch inline-prompt 
-                                :active-color="item.option.activeColor" :active-text="item.option.activeText" 
-                                :inactive-color="item.option.inactiveColor" :inactive-text="item.option.inactiveText"
-                                :v-model="`scope.row.${item.prop}`"
+                            <el-switch @change="$emit('onSwitchChange',scope.row)" inline-prompt 
+                                :active-value="item.option.activeValue" :active-color="item.option.activeColor" :active-text="item.option.activeText" 
+                                :inactive-value="item.option.inactiveValue" :inactive-color="item.option.inactiveColor" :inactive-text="item.option.inactiveText"
+                                v-model="scope.row[item.prop]"
                             ></el-switch>
                         </template>
                     </el-table-column>
+                    <!-- el-progress  -->
+                    <el-table-column v-else-if="item.type == 'progress'" show-overflow-tooltip 
+                        :label="item.label" :align="item.align != null ? item.align : 'center'" :width="item.width">
+                        <template #default="scope">
+                            <el-progress :percentage="scope.row[item.prop]" 
+                            :status="item.option.status"
+                            :color="item.option.color"/>
+                        </template>
+                    </el-table-column>
+                    <!-- 其他数据列 -->
                     <el-table-column v-else show-overflow-tooltip 
                         :prop="item.alias==null?item.prop:item.alias" 
                         :label="item.label" 
@@ -37,15 +58,10 @@
                         :fixed="item.fixed">
                     </el-table-column>
                 </template>
+                <!-- 自定义插槽  -->
                 <el-table-column v-if="operation" fixed="right" label="操作" align="center" width="220">
                     <template #default="scope">
-                        <el-button v-if="operation.add" type="text" @click="$emit('onAdd', scope.row)" style="color: var(--add)">添加</el-button>
-                        <el-button v-if="operation.detail" type="text" @click="$emit('onDetail', scope.row)" style="color: var(--query)">查看</el-button>
-                        <el-button v-if="operation.upper && (scope.row['status'] == 0 || scope.row['status'] == 2)" type="text" @click="$emit('onUpper', scope.row)" style="color: var(--upper)">上架</el-button>
-                        <el-button v-if="operation.down && scope.row['status'] == 1" type="text" @click="$emit('onDown', scope.row)" style="color: var(--down)">下架</el-button>
-                        <el-button v-if="operation.edit" type="text" @click="$emit('onEdit', scope.row)" style="color: var(--edit)">编辑</el-button>
-                        <el-button v-if="operation.del" type="text" @click="$emit('onDelete', scope.row)" style="color: var(--delete)">删除</el-button>
-                        <el-button v-if="operation.role" type="text" @click="$emit('onRole', scope.row)" style="color: var(--role)">权限</el-button>
+                        <slot name="column" v-bind:column="scope"></slot>
                     </template>
                 </el-table-column>
             </el-table>
@@ -64,21 +80,48 @@
     import {defineProps, reactive, toRefs} from 'vue'
     
     const prop = defineProps({
-        loading: Boolean,
-        page: Object,
-        column: Array,
-        operation: Object,
-        tableHeight: Number,
-        data: Array,
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        page: {
+            type: Object,
+            default:{
+                current: 1,
+                size: 10,
+                total: 0,
+            }
+        }, 
+        column: {
+            type: Array,
+            default: []
+        },
+        operation: { // 操作列，自定义插槽
+            type: Boolean,
+            default: false
+        }, 
+        tableHeight: {
+            type: Number,
+            default: 520
+        },
+        headerCellStyle: {
+            type: Object,
+            default:{color:'#606266',fontWeight: 700,background:'var(--bg3)'}
+        },
+        data: {
+            type: Array,
+            default: []
+        },
     })
 
     const emit = defineEmits([
-        'onAdd','onDetail','onUpper','onDown','onEdit','onDelete','onRole',
-        'onSizeChange','onCurrentChange','onSelectionChange','setCellColor'
+        'onSizeChange','onCurrentChange','onSelectionChange','setCellColor',
+        'onButtonClick','onSwitchChange',
     ])
 
     const state = reactive({
-        tableHeight: prop.tableHeight?prop.tableHeight:520,
+        tableHeight: prop.tableHeight,
+        headerCellStyle: prop.headerCellStyle,
         cellStyle: function(e){
             let obj = {};
             emit('setCellColor', e, (color = {}) =>{
@@ -88,11 +131,7 @@
             return obj;
         }
     })
-    const {tableHeight,cellStyle} = toRefs(state)
-
-    function valFormat(row,val){
-        return eval(`row.${val}`);
-    }
+    const {tableHeight,headerCellStyle,cellStyle} = toRefs(state)
 </script>
-<style lang="less">
+<style lang="less" scoped>
 </style>
--- tansci-view/src/views/system/Menu.vue
+++ tansci-view/src/views/system/Menu.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="menu">
-		<Table :data="tableData" :column="tableTitle" :operation="operation" :page="page" :loading="loading" :tableHeight="tableHeight"
-            @onSizeChange="onSizeChange" @onCurrentChange="onCurrentChange" @setCellColor="setCellColor"
-            @onAdd="onAdd">
+		<Table :data="tableData" :column="tableTitle" :operation="true" :page="page" :loading="loading" :tableHeight="tableHeight"
+            @onSizeChange="onSizeChange" @onCurrentChange="onCurrentChange" @setCellColor="setCellColor" @onButtonClick="onButtonClick">
+            <template #search>
+                <div>111</div>
+            </template>
+            <template #column="scope">
+                <el-button @click="aaa(scope)" type="primary">Primary</el-button>
+            </template>
 		</Table>
     </div>
 </template>
@@ -11,13 +16,6 @@
 	import Table from '../../components/Table.vue'
     const state = reactive({
         loading: false,
-        operation: {
-            add: true,
-            edit: true,
-            del: true,
-            upper: true,
-            down: true,
-        },
         page: {
             current: 1,
             size: 10,
@@ -27,11 +25,11 @@
         tableTitle: [
             {prop:'name',label:'名称',align:'left'},
             {prop:'chineseName',label:'中文名称',type:'tag',option:{effect:'dark',type:'danger',size:'mini'}},
-            {prop:'englishName',label:'英文名称'},
+            {prop:'englishName',label:'英文名称',type:'button',option:{size:'mini'}},
             {prop:'status',alias:'statusName',label:'状态',type:'switch',option:{
-                activeColor:'#13ce66',activeText:'否',inactiveColor:'#ff4949',inactiveText:'是'}
+                activeValue:1,activeColor:'#13ce66',inactiveValue:0,activeText:'否',inactiveColor:'#ff4949',inactiveText:'是'}
             },
-            {prop:'type',alias:'typeName',label:'类型'},
+            {prop:'type',alias:'typeName',label:'类型',type:'progress',option:{color:'#000fff'}},
             {prop:'path',label:'路由'},
             {prop:'sort',label:'排序'},
             {prop:'updateTime',label:'更新时间'},
@@ -45,8 +43,8 @@
 
     onMounted(()=>{
         state.tableData = [
-            {name:'测试01',chineseName:'测试01',englishName:'测试01',status:1,type:1,path:'测试01',sort:1,updateTime:'2021-12-13 15:00:14',createTime:'2021-12-13 15:00:14'},
-            {name:'测试02',chineseName:'测试01',englishName:'测试01',status:1,type:1,path:'测试01',sort:1,updateTime:'2021-12-13 15:00:14',createTime:'2021-12-13 15:00:14'},
+            {name:'测试01',chineseName:'测试01',englishName:'测试01',status:1,type:10,path:'测试01',sort:1,updateTime:'2021-12-13 15:00:14',createTime:'2021-12-13 15:00:14'},
+            {name:'测试02',chineseName:'测试02',englishName:'测试01',status:0,type:100,path:'测试01',sort:1,updateTime:'2021-12-13 15:00:14',createTime:'2021-12-13 15:00:14'},
         ]
     })
 
@@ -73,6 +71,14 @@
         } else {
             callback({});
         }
+    }
+
+    const onButtonClick = (row) =>{
+        console.log(row)
+    }
+
+    const aaa = (row) => {
+        console.log(row)
     }
 </script>
 <style lang="less" scoped>
