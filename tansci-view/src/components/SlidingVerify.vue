94ac3f2895748c841c47aa762673f68f428ff11d
tanyp <typ1805@qq.com>
2021-12-30 08:46:50 +0800

    登录添加滑动验证码

---
+++ tansci-view/src/components/SlidingVerify.vue
@@ -0,0 +1,147 @@
+<template>
+    <div v-if="verifyRefresh" class="sliding-verify">
+        <div class="slider" :class="rangeStatus?'success':''" >
+            <el-icon @mousedown.self="onMove">
+                <component :is="rangeStatus?successIcon:startIcon"></component>
+            </el-icon>
+            {{rangeStatus?successText:startText}}
+        </div>
+    </div>
+</template>
+<script setup>
+    import {defineProps, reactive, toRefs, nextTick} from 'vue'
+
+    const prop = defineProps({
+        // 成功函数
+		successFun: {
+			type: Function
+		},
+		// 成功图标
+		successIcon: {
+			type: String,
+			default: 'CircleCheckFilled'
+		},
+		// 成功文字
+		successText: {
+			type: String,
+			default: '验证成功'
+		},
+		// 开始的图标
+		startIcon: {
+			type: String,
+			default: 'DArrowRight'
+		},
+		// 开始的文字
+		startText: {
+			type: String,
+			default: '请拖动滑块验证'
+		},
+		// 失败函数
+		errorFun: {
+			type: Function
+		},
+		// 用值来进行监听
+		status: {
+			type: Boolean,
+            default: false
+		}
+    })
+
+    const state = reactive({
+        verifyRefresh: true,
+        disX: 0,
+		rangeStatus: prop.status
+    })
+
+    const {verifyRefresh,disX,rangeStatus} = toRefs(state)
+
+    // 滑块移动
+    function onMove (e){
+        let ele = e.target;
+        let startX = e.clientX;
+        let eleWidth = ele.offsetWidth;
+        let parentWidth =  ele.parentElement.offsetWidth;
+        let MaxX = parentWidth - eleWidth -2;
+        // 不运行
+        if(state.rangeStatus){
+            return false;
+        }
+        document.onmousemove = (e) => {
+            let endX = e.clientX;
+            state.disX = endX - startX;
+            if(state.disX <= 0){
+                state.disX = 0;
+            }
+            // 减去滑块的宽度,体验效果更好
+            if(state.disX >= MaxX - eleWidth){
+                state.disX = MaxX;
+            }
+            ele.style.transition = '.1s all';
+            ele.style.transform = 'translateX('+state.disX+'px)';
+            e.preventDefault();
+        }
+        document.onmouseup = ()=> {
+            if(state.disX !== MaxX){
+                ele.style.transition = '.5s all';
+                ele.style.transform = 'translateX(0)';
+                // 执行失败的函数
+                prop.errorFun(false);
+            }else{
+                state.rangeStatus = true;
+                // 执行成功的函数
+                prop.successFun(true);
+            }
+            document.onmousemove = null;
+            document.onmouseup = null;
+        }
+    }
+
+    // 刷新
+    const onRefresh = () =>{
+        state.verifyRefresh = false;
+        nextTick(()=>{
+            state.rangeStatus = false;
+            state.verifyRefresh = true;
+        })
+    }
+
+    defineExpose({
+        onRefresh
+    })
+</script>
+<style scoped lang="less">
+    .sliding-verify{
+        .slider{
+            background-color: #EBEEF5;
+            position: relative;
+            transition: 1s all;
+            user-select: none;
+            color: #909399;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 32px; 
+            border: 1px solid #dcdfe6;
+        }
+        .slider i{
+            position: absolute;
+            left: 0;
+            width: 34px; 
+            height: 100%;
+            color: #919191;
+            background-color: #fff;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .slider.success{
+            background-color: #7AC23C;
+            color: #fff;
+            right: 0;
+        }
+        .slider.success i{
+            color: #7AC23C;
+        }
+    }
+</style>
--- tansci-view/src/views/Login.vue
+++ tansci-view/src/views/Login.vue
@@ -3,7 +3,7 @@
 		<el-card shadow="always">
 			<div class="login-main">
 				<div class="login-logo">
-					<el-image src="./src/assets/image/login-logo.png" style="width: 100%; height:100%"></el-image>
+					<el-image src="./src/assets/image/login-logo.png"  style="width: 100%; height:100%"></el-image>
 				</div>
 				<div class="login-form">
 					<el-form :model="loginForm" :rules="rules" ref="loginRuleForm">
@@ -17,6 +17,9 @@
 								{required: true,message: '请输入密码',trigger: 'blur'},
 								{pattern: /^[a-zA-Z]\w{5,17}$/,message: '密码格式有误，请重新输入',trigger: 'blur'}]">
 							<el-input type="password" v-model="loginForm.password" prefix-icon="Lock" show-password placeholder="请输入密码" style="width:100%"></el-input>
+						</el-form-item>
+						<el-form-item prop="verifyStatus" :rules="[{required: true,message: '请拖动滑块验证',trigger: 'blur'}]">
+							<SlidingVerify ref="slidingVerify" :status="loginForm.verifyStatus" :successFun="onVerifySuccess" :errorFun="onVerifyError"></SlidingVerify>
 						</el-form-item>
 						<el-form-item>
 							<el-checkbox v-model="loginForm.keepPassword" label="记住密码"></el-checkbox>
@@ -43,10 +46,12 @@
 	import {useRouter} from 'vue-router'
 	import {useStore} from 'vuex'
 	import {login,menuList} from '../api/systemApi'
+	import SlidingVerify from '../components/SlidingVerify.vue'
 
 	const store = useStore()
 	const router = useRouter()
 	let loginRuleForm = ref(null) 
+	let slidingVerify = ref()
 	const state = reactive({
 		loginStyle: {
 			height: '',
@@ -54,6 +59,7 @@
 		loginForm: {
 			username: '',
 			password: '',
+			verifyStatus: null,
 			keepPassword: null,
 		},
 	})
@@ -64,15 +70,17 @@
 		state.loginStyle.height = (document.body.clientHeight || document.documentElement.clientHeight) + "px"
 	})
 
+	const onVerifySuccess = (e) =>{
+		state.loginForm.verifyStatus = e;
+	}
+	const onVerifyError = (e) =>{
+		state.loginForm.verifyStatus = e;
+	}
+
 	const submit = async () => {
 		const form = unref(loginRuleForm)
 		if (!form) return;
 		await form.validate();
-
-		// if(state.loginForm.code <= 0){
-		// 	ElMessage.warning('请拖动滑块进行验证！')
-		// 	return false;
-		// }
 
 		// 登录成功后设置token到vuex中
 		let param = {
@@ -89,6 +97,10 @@
 					router.push({path: 'home'});
 				})
 			}
+		}).catch(()=>{
+			state.loginForm.verifyStatus = null;
+			console.log(slidingVerify)
+			slidingVerify.value.onRefresh()
 		})
 	}
 </script>
